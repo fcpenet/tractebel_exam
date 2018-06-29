16913bfd5baa4993a0555b30f3820009d139803d
Francis Penetrante <fcpenet@gmail.com>
2018-06-29 19:11:55 +0800

    removed logging of non test related

--- Backend/app.js
+++ Backend/app.js
@@ -2,13 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var env = process.env.NODE_ENV || 'development';
 
 var indexRouter = require('./routes/index');
 var clientsRouter = require('./routes/clients');
 
 var app = express();
 
-app.use(logger('dev'));
+if(env !== 'test'){
+	app.use(logger('dev'));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
